15a0769669f6d48e53745b067240cfab0df1ff4e
Lysko910 <mymisie2@gmail.com>
2023-05-10 19:45:12 +0200

    test decorators

--- src/main.cpp
+++ src/main.cpp
@@ -20,33 +20,91 @@ int main()
 	//wpisanie odczytanej konfiguracji
 	cout<<arx<<endl;
 
-	//symulacja odpowiedzi na skok jednostkowy
-	constexpr size_t LICZ_ITER = 50;
-    std::vector<double> sygWe(LICZ_ITER);      // pobudzenie modelu, 
-    std::vector<double> faktSygWy(LICZ_ITER);  // faktyczna sekwencja wy
+ /**
+  * \class Generator (component)
+  * @brief Klasa interface dla genertorow i ich dekoratorow
+  */
+class Generator{
+public:
+virtual ~Generator(){};
+virtual double generuj() = 0;
 
-    // Symulacja skoku jednostkowego w chwili 1. (!!i - daje 1 dla i != 0);
-    for (int i = 0; i < LICZ_ITER; i++)
-        sygWe[i] = !!i;
+};
 
-	std::cout<< "ARX"<<std::endl;
-    // Symulacja modelu na skok 
-    for (int i = 0; i < LICZ_ITER; i++){
-        faktSygWy[i] = arx.symuluj(sygWe[i]);
-		std::cout<< faktSygWy[i] <<std::endl;
+/**
+  * \class Generator (concrete_component)
+  * @brief Klasa interface dla genertorow i ich dekoratorow
+  */
+class GeneratorConst: public Generator{
+private:
+double m_val;
+public:
+GeneratorConst(double n_val = 0.0):m_val(n_val){};
+double generuj()override{
+	return this->m_val;
+}
+
+};
+
+/**
+  * \class Decorator (interface do dekoratorw)
+  * @brief Klasa interface dla dekoratorow
+  * */
+class Decorator :public Generator{
+protected:
+  Generator* m_generator;
+  unsigned int m_licznik_wywolan = 0;
+public:
+  Decorator(Generator* n_generator): m_generator(n_generator){
+  };
+
+  double generuj()override{
+	return this->m_generator->generuj();
+}
+	
+};
+
+/**
+  * \class Decorator  prostokata(interface do dekoratorw)
+  * @brief Klasa implementujaca konkretny dekorator typu prostokat
+  * */
+class DecoratorProstokat:public Decorator{
+private:
+unsigned int m_T;
+public:
+DecoratorProstokat(Generator* n_generator,unsigned int n_T):Decorator(n_generator),m_T(n_T){  
+}
+
+double generuj()override{
+	double val = Decorator::generuj();
+  double half_T = m_T/2.0;
+  if(m_licznik_wywolan >m_T){
+    m_licznik_wywolan= 0;
+  }else{
+    m_licznik_wywolan++;
+  }
+      if(m_licznik_wywolan <=half_T){
+        return val;
+      }else if(m_licznik_wywolan>half_T){
+        return 0.0;
+      }
+}
+
+};
+
+
+
+
+int main()
+{	
+  Generator* g =new GeneratorConst(1.0);
+  Generator* d1 = new DecoratorProstokat(g,10);
+	for(int i=0;i<100;i++){
+  double val = d1->generuj();
+	std::cout<<val<<std::endl;	
 	}
-	// regulacja PI
-	PI pid(2.5);
-    std::vector<double> Y(LICZ_ITER);  // faktyczna sekwencja wy
- 	std::cout<< "ARX + PID"<<std::endl;
-	for (int i = 1; i < LICZ_ITER; i++){
-		double u = pid.symuluj(Y[i-1]);
-		std::cout<< u <<"\t";
-        Y[i] = arx.symuluj(u);
-		std::cout<< Y[i]<<std::endl;
-	}
 
 
-	plotToFile<double>(faktSygWy,Y);
+	//plotToFile<double>(signal);
     return 0;
 }
