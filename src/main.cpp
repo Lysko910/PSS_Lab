f51decea23c0705250760f6b3ad13432babda19a
Lysko910 <mymisie2@gmail.com>
2023-06-07 12:41:12 +0200

    Koncowy test LAB

--- src/main.cpp
+++ src/main.cpp
@@ -14,37 +14,44 @@ using std::endl;
 
 int main(void){	
   //twirzenie generatorow
-  Generator* generator_const =new GeneratorConst(1.0);
+  Generator* generator_const =new GeneratorConst(3.0);
   Generator* generator_skok = new DecoratorDelay(generator_const,10);
   
   //twirzenie obiektow
-  Komponent * objekt1 = new ARX({ -0.8, 0.4 }, { 1 }, 1, 0.0);
-  Komponent * objekt2 = new ARX({ -0.8, 0.4 }, { 1 }, 1, 0.0);
+  Komponent * objekt1 = new PID(1.2, 0.05, 0.01, 1.0);
+  Komponent * objekt2 = new ARX({-0.6 }, { 1 }, 0, 0.0);
+  Komponent * objekt3 = new ARX({-0.3 }, { 1 }, 0, 0.0);
+
  
+   Komponent * struktura2 = new StrukturaRownolegla();
+    struktura2->dodaj(objekt3);
+    struktura2->dodaj(objekt2);
  // utworzenie struktury szeregowej z obiektami
   Komponent * struktura1 = new StrukturaSzeregowa();
   struktura1->dodaj(objekt1);
-  struktura1->dodaj(objekt2);
+  struktura1->dodaj(struktura2);
+
   //test struktura szeregowa
-  for(int i=0;i<100;i++){
-  double val = generator_skok->generuj();
-  val = struktura1->symuluj(val);
-	std::cout<<val<<std::endl;	
-	}
-// utworzenie struktury rownoleglej z obiektami
- Komponent * struktura2 = new StrukturaRownolegla();
-  struktura2->dodaj(objekt1);
-  struktura2->dodaj(objekt2);
-  //test struktura rwnolegla
-  for(int i=0;i<100;i++){
-  double val = generator_skok->generuj();
-  val = struktura2->symuluj(val);
-	std::cout<<val<<std::endl;	
+  // wartosc zadana
+
+  double w =0.0;
+  double y =0.0;
+  double e =0.0;
+  //  PID->ARX
+  for(int i=0;i<300;i++){
+    // wartosc zadana
+  w = generator_const->generuj();
+  // Wezel e = w-y;
+  e = w - y;
+  y = struktura1->symuluj(e);
+	std::cout<<y<<std::endl;	
 	}
 	
   delete generator_const;
   delete generator_skok;
   delete objekt1;
+  delete objekt2;
+   delete objekt3;
   delete struktura1;
   delete struktura2;
     return 0;
